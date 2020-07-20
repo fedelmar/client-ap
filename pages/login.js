f7040c03e843f2f3a6b3ba6683ea5608548871fa
Federico Suarez <fsuarez91@gmail.com>
2020-07-20 11:11:54 -0300

    Routing de registro y login

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Login = () => {
+    return (
+        <>
+            <Layout>
+            <h1 className="text-center text-2xl text-white font-ligth">Login</h1> 
+            </Layout>
+        </>
+    );
+}
+
+export default Login;
---
+++ pages/registro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Registro = () => {
+    return (
+        <>
+            <Layout>
+            <h1 className="text-center text-2xl text-white font-ligth">Registro</h1> 
+            </Layout>
+        </>
+    );
+}
+
+export default Registro;
