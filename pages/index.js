8452a7512cc9bc72d0b7fef43fd4c799444e96d0
Federico Suarez <fsuarez91@gmail.com>
2020-07-21 11:22:11 -0300

    Mostrar datos de clientes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { gql, useQuery } from '@apollo/client'
 const OBTENER_CLIENTES   = gql `
 query obtenerClientesVendedor {
     obtenerClientesVendedor{
+        id
         nombre
         apellido
         empresa
@@ -18,9 +19,9 @@ export default function Index() {
   // Consulta de apollo
   const { data, loading, error} = useQuery(OBTENER_CLIENTES);
 
-  console.log(data);
-  console.log(loading);
-  console.log(error);
+  //console.log(data);
+  //console.log(loading);
+  //console.log(error);
 
   if(loading) return (
     <Layout>
@@ -31,6 +32,26 @@ export default function Index() {
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Inicio</h1>
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Nombre</th>
+              <th className="w-1/5 py-2">Empresa</th>
+              <th className="w-1/5 py-2">Email</th> 
+            </tr>
+          </thead>
+
+          <tbody className="bg-white">
+            {data.obtenerClientesVendedor.map( cliente => (
+              <tr key={cliente.id}>
+                <td className="border px-4 py-2 ">{cliente.nombre} {cliente.apellido} </td>
+                <td className="border px-4 py-2 ">{cliente.empresa}</td>
+                <td className="border px-4 py-2 ">{cliente.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      
       </Layout>
     </div>
   )
