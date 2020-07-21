2bd6a264968ca95fdb22edb5986b4c95e31d9f9e
Federico Suarez <fsuarez91@gmail.com>
2020-07-21 10:57:50 -0300

    Obtener data del servidor

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
 import Head from 'next/head'
 import Layout from '../components/Layout';
+import { gql, useQuery } from '@apollo/client'
+
+const OBTENER_CLIENTES   = gql `
+query obtenerClientesVendedor {
+    obtenerClientesVendedor{
+        nombre
+        apellido
+        empresa
+        email
+      }
+    }
+`;
 
 export default function Index() {
+
+  // Consulta de apollo
+  const { data, loading, error} = useQuery(OBTENER_CLIENTES);
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
+  if(loading) return (
+    <Layout>
+      <p className="text-2xl text-gray-800 font-light" >Cargando...</p>
+    </Layout>);
+
   return (
     <div>
       <Layout>
