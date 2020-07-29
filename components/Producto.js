8f49b8f76a66a5627fcc1ca521ddf321ac73d249
fedelmar <fsuarez91@gmail.com>
2020-07-29 11:43:40 -0300

    Actualizar un producto

--- components/Producto.js
+++ components/Producto.js
@@ -74,6 +74,13 @@ const Producto = ({producto}) => {
           })
     }
 
+    const editarProducto = () => {
+        Router.push({
+            pathname: "/editarProducto/[id]",
+            query: { id }
+        })
+    }
+
 
     return (
         <tr>
@@ -94,7 +101,7 @@ const Producto = ({producto}) => {
                 <button
                     type="button"
                     className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-                    //onClick={() => editarProducto() }
+                    onClick={() => editarProducto()}
                 >
                     Editar
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-4 h-4 ml-2"><path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
