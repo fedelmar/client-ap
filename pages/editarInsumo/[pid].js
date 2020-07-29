f11e0b91d0eedc1e1393052a1bc8dae3145ae947
fedelmar <fsuarez91@gmail.com>
2020-07-29 15:24:39 -0300

    Manejo de insumos (Listar, crear nuevo, eliminar y editar)

---
+++ pages/editarInsumo/[pid].js
@@ -0,0 +1,195 @@
+import React from 'react';
+import { useRouter } from 'next/router'
+import Layout from '../../components/Layout';
+import { Formik } from 'formik'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import * as Yup from 'yup'
+import Swal from 'sweetalert2';
+
+const OBTENER_INSUMO = gql`
+   query obtenerInsumo ($id: ID!) {
+    obtenerInsumo (id: $id) {
+      id
+      nombre
+      categoria
+      cantidad
+    }
+  }
+`;
+
+const ACTUALIZAR_INSUMO = gql`
+    mutation actualizarInsumo($id: ID!, $input: InsumoInput) {
+        actualizarInsumo(id: $id, input: $input) {
+            nombre
+            categoria
+            cantidad
+        }
+}
+`;
+
+const EditarInsumo = () => {
+
+    const router = useRouter();
+    const { query: { id } } = router;
+
+    const { data, loading, error } = useQuery(OBTENER_INSUMO, {
+        variables: {
+            id
+        }
+    });
+
+    const [ actualizarInsumo ] = useMutation( ACTUALIZAR_INSUMO );
+
+    const schemaValidacion = Yup.object({
+        nombre: Yup.string(),
+        categoria: Yup.string(),
+        cantidad: Yup.number() 
+                    
+    });
+    
+    if(loading) return 'Cargando...';
+
+    const { obtenerInsumo } = data;
+
+    const actualizarInfoInsumo = async valores => {
+        const { nombre, categoria, cantidad } = valores;
+
+        try {
+            const { data } = await actualizarInsumo({
+                variables: {
+                    id,
+                    input: {
+                        nombre, 
+                        categoria,
+                        cantidad
+                    }
+                }
+            });
+
+            // console.log(data);
+  
+            // Mostrar Alerta
+            Swal.fire(
+                'Actualizado',
+                'El insumo se actualizó correctamente',
+                'success'
+            )
+
+            // Redireccionar
+            router.push('/insumos');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <Layout>
+          <h1 className="text-2xl text-gray-800 font-light">Editar Insumo</h1>
+
+          <div className="flex justify-center mt-5">
+                <div className="w-full max-w-lg">
+
+                    <Formik
+                        validationSchema={ schemaValidacion }
+                        enableReinitialize
+                        initialValues={ obtenerInsumo }
+                        onSubmit={ ( valores ) => { actualizarInfoInsumo(valores) }}
+                    >
+
+                    {props => {
+                    console.log(props);
+                    return (
+                            <form
+                                className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                                onSubmit={props.handleSubmit}
+                            >
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                            Nombre
+                                        </label>
+
+                                        <input
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            id="nombre"
+                                            type="text"
+                                            placeholder="Nombre del insumo"
+                                            onChange={props.handleChange}
+                                            onBlur={props.handleBlur}
+                                            value={props.values.nombre}
+                                        />
+                                    </div>
+
+                                    { props.touched.nombre && props.errors.nombre ? (
+                                        <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                            <p className="font-bold">Error</p>
+                                            <p>{props.errors.nombre}</p>
+                                        </div>
+                                    ) : null  }
+
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria">
+                                            Categoria
+                                        </label>
+                                        <select
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            name="categoria"
+                                            id="categoria"
+                                            type="text"
+                                            onChange={props.handleChange}
+                                            onBlur={props.handleBlur}
+                                        >
+                                            <option value={props.values.categoria} label={props.values.categoria} />
+                                            <option value="Esponjas" label="Esponjas" />
+                                            <option value="Placas" label="Placas" />
+                                            <option value="Geles" label="Geles" />
+                                            <option value="Sistemas" label="Sistemas" />
+                                        </select>
+                                    </div>
+
+                                    { props.touched.categoria && props.errors.categoria ? (
+                                        <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                            <p className="font-bold">Error</p>
+                                            <p>{props.errors.categoria}</p>
+                                        </div>
+                                    ) : null  }
+
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="cantidad">
+                                            Cantidad
+                                        </label>
+
+                                        <input
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            id="cantidad"
+                                            type="number"
+                                            placeholder="Cantidad"
+                                            onChange={props.handleChange}
+                                            onBlur={props.handleBlur}
+                                            value={props.values.cantidad}
+                                        />
+                                    </div>
+
+                                    { props.touched.cantidad && props.errors.cantidad ? (
+                                        <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
+                                            <p className="font-bold">Error</p>
+                                            <p>{props.errors.cantidad}</p>
+                                        </div>
+                                    ) : null  }
+
+
+                                    <input
+                                        type="submit"
+                                        className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
+                                        value="Editar Insumo"
+                                    />
+                            </form>      
+                        )
+                    }}
+                    </Formik> 
+                </div>
+            </div> 
+        </Layout>        
+    );
+}
+
+export default EditarInsumo
--- pages/insumos.js
+++ pages/insumos.js
@@ -1,10 +1,65 @@
 import Layout from '../components/Layout';
+import Insumo from '../components/Insumo';
+import { gql, useQuery } from '@apollo/client';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const OBTENER_INSUMOS = gql`
+   query obtenerInsumos {
+    obtenerInsumos{
+      id
+      nombre
+      categoria
+      cantidad
+    }
+  }
+`;
 
 export default function Pedidos() {
+
+  const router = useRouter();
+  
+  const { data, loading, error } = useQuery(OBTENER_INSUMOS);
+
+  if(loading) return (
+    <Layout>
+      <p className="text-2xl text-gray-800 font-light" >Cargando...</p>
+    </Layout>
+  );
+
+  if(!data) {
+    return router.push('/login');
+  }
+
   return (
     <div>
       <Layout>
-      <h1 className="text-2xl text-gray-800 font-light">Insumos</h1>
+        <h1 className="text-2xl text-gray-800 font-light">Insumos</h1>
+
+        <Link href="/nuevoinsumo">
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Nuevo Insumo</a>
+        </Link>
+
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Nombre</th>
+              <th className="w-1/5 py-2">Categoria</th>
+              <th className="w-1/5 py-2">Cantidad</th>
+              <th className="w-1/5 py-2">Eliminar</th>
+              <th className="w-1/5 py-2">Editar</th>   
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {data.obtenerInsumos.map( insumo => (
+                <Insumo
+                  key={insumo.id}
+                  insumo={insumo}
+                />
+            ))}
+          </tbody>  
+        </table>
+
       </Layout>
     </div>
   )
