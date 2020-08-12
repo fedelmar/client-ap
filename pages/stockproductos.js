f958a10b2be608696431541165b426a41c60f23a
fedelmar <fsuarez91@gmail.com>
2020-08-12 10:38:55 -0300

    Crear nuevo lote en Stock de productos

--- pages/stockproductos.js
+++ pages/stockproductos.js
@@ -38,7 +38,7 @@ const StockProductos = () => {
     <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Stock de Productos</h1>
 
-        <Link href="">
+        <Link href="nuevoloteproducto">
           <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Nuevo Lote</a>
         </Link>
 
