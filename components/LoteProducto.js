4d87cb6105d8dcb5e48dddab59f3e7690213bde2
fedelmar <fsuarez91@gmail.com>
2020-08-12 09:14:01 -0300

    Eliminar lote en stock de productos

--- components/LoteProducto.js
+++ components/LoteProducto.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import Swal from 'sweetalert2';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 const LISTADO_PRODUCTOS = gql`
     query obtenerProductos {
@@ -9,18 +10,81 @@ const LISTADO_PRODUCTOS = gql`
             nombre
         }
     }
-`; 
+`;
+
+const ELIMINAR_LOTE = gql `
+    mutation eliminarProductoStock($id: ID!){
+        eliminarProductoStock(id: $id)
+    }
+`;
+
+const LISTADO_STOCK = gql `
+    query obtenerProductosStock{
+        obtenerProductosStock{
+            id
+        }
+    }
+`;
 
 const LoteProducto = ({loteProducto, rol}) => {
+    
+    const { lote, cantidad, producto, estado, id}  = loteProducto;
 
     const {data, loading} = useQuery(LISTADO_PRODUCTOS);
-    const { lote, cantidad, producto, estado }  = loteProducto;
+
+    const [eliminarProductoStock] = useMutation(ELIMINAR_LOTE, {
+        update(cache) {
+            // Obtener copia del stock
+            const { obtenerProductosStock } = cache.readQuery({ query: LISTADO_STOCK });
+           
+            // Reescribir el cache
+            cache.writeQuery({
+                query: LISTADO_STOCK,
+                data: {
+                    obtenerProductosStock : obtenerProductosStock.filter( lote => lote.id !== id )
+                }
+            })
+        }
+    });
+
 
     if (loading) return null;
 
-    console.log(data.obtenerProductos);
-    const {nombre} = data.obtenerProductos.find(i => i.id === producto) 
-    console.log('nombre ', nombre)
+    // Buscar dentro de lista de productos el nombre del producto
+    const {nombre} = data.obtenerProductos.find(i => i.id === producto);
+
+    const confirmarEliminarLote = async () => {
+        Swal.fire({
+            title: '¿Desea eliminar el lote?',
+            text: "Sera eliminado definitivamente",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar!',
+            cancelButtonText: 'Cancelar'
+          }).then( async (result) => {
+            if (result.value) {
+
+                try {
+                    //Eliminar por id
+                    const { data } = await eliminarProductoStock({
+                        variables: {
+                            id
+                        }
+                    })
+                    //console.log(data);
+                    Swal.fire(
+                        'Eliminado!',
+                        data.eliminarProductoStock,
+                        'success'
+                )
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+          })
+    };
 
     return ( <tr>
         <th className="border px-4 py-2" >{lote}</th>
@@ -41,7 +105,7 @@ const LoteProducto = ({loteProducto, rol}) => {
                 <td className="border px-4 py-2 ">
                     <button
                         type="button"
-                       // onClick={() => confimarEliminarProducto()}
+                        onClick={() => confirmarEliminarLote()}
                         className="flex justify-center item-center bg-red-800 py-2 px-4 w-full text-white rounded uppercase font-bold text-xs"    
                     >
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-4 h-4"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
