6c611f4c275cc721009708ffb40bffb54c8ef875
fedelmar <fsuarez91@gmail.com>
2020-08-12 14:22:22 -0300

    Listar stock de insumos

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -34,6 +34,11 @@ const Sidebar = () => {
                         <a className="text-white block">Stock de Productos</a>
                     </Link>
                 </li>
+                <li className={router.pathname === "/stockinsumos" ? "bg-blue-800 p-2" : "p-2"}>    
+                    <Link href="/stockinsumos">
+                        <a className="text-white block">Stock de Insumos</a>
+                    </Link>
+                </li>
                 {/*<li className={router.pathname === "/pedidos" ? "bg-blue-800 p-2" : "p-2"}>    
                     <Link href="/pedidos">
                         <a className="text-white block">Pedidos</a>
---
+++ pages/nuevoloteinsumo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NuevoLoteInsumo = () => {
+
+    return (
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Nuevo Lote de Insumos</h1>
+        </Layout>
+    );
+}
+
+export default NuevoLoteInsumo
