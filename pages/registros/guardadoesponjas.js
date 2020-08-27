b112f04f51a45dbebf572ea4d27eb6c2698d2a59
fedelmar <fsuarez91@gmail.com>
2020-08-27 10:32:15 -0300

    Eliminar registro en GE, y crear pagina de nuevo registro

--- pages/registros/guardadoesponjas.js
+++ pages/registros/guardadoesponjas.js
@@ -3,6 +3,7 @@ import UsuarioContext from '../../context/usuarios/UsuarioContext';
 import Layout from '../../components/Layout';
 import {gql, useQuery} from '@apollo/client';
 import RegistroGE from '../../components/RegistroGE'
+import Link from 'next/link'
 
 const LISTA_REGISTROS = gql `
     query obtenerRegistrosGE{
@@ -23,7 +24,6 @@ const LISTA_REGISTROS = gql `
         }
 `;
 
-
 const GuardadoEsponjas = () => {
 
     const usuarioContext = useContext(UsuarioContext);
@@ -43,39 +43,43 @@ const GuardadoEsponjas = () => {
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">Guardado de Esponjas</h1>
 
-            <div className="overflow-x-scroll">
-          <table className="table-auto shadow-md mt-10 w-full w-lg">
-            <thead className="bg-gray-800">
-                <tr className="text-white">
-                    <th className="w-1/12 py-2">Fecha</th>
-                    <th className="w-2/12 py-2">Horario</th>
-                    <th className="w-1/12 py-2">Producto</th>
-                    <th className="w-1/12 py-2">Lote</th>
-                    <th className="w-1/12 py-2">Tipo de Caja</th>
-                    <th className="w-1/12 py-2">Descarte de caja</th>
-                    <th className="w-1/12 py-2">Esponjas</th>
-                    <th className="w-1/12 py-2">Descarte</th>
-                    <th className="w-1/12 py-2">Operario</th>
-                    <th className="w-1/12 py-2">Observaciones</th>
-                    {rol === "Admin" ? (
-                    <>
-                        {/* De momento la edicion no va a estar disponible
+            <Link href="/registros/nuevoregistroGE">
+                <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Iniciar Registro</a>
+            </Link>
 
-                        <th className="w-1/12 py-2">Editar</th> */}
-                        <th className="w-1/12 py-2">Eliminar</th>
-                    </>                  
-                    ) : null}   
-                </tr>
-            </thead>
-            <tbody className="bg-white">
-              {data.obtenerRegistrosGE.map(registro => (
-                  <RegistroGE
-                    key={registro.id}
-                    registro={registro}
-                    rol={rol}
-                  />
-              ))}  
-            </tbody>  
+            <div className="overflow-x-scroll">
+            <table className="table-auto shadow-md mt-10 w-full w-lg">
+                <thead className="bg-gray-800">
+                    <tr className="text-white">
+                        <th className="w-1/12 py-2">Fecha</th>
+                        <th className="w-2/12 py-2">Horario</th>
+                        <th className="w-1/12 py-2">Producto</th>
+                        <th className="w-1/12 py-2">Lote</th>
+                        <th className="w-1/12 py-2">Tipo de Caja</th>
+                        <th className="w-1/12 py-2">Descarte de caja</th>
+                        <th className="w-1/12 py-2">Esponjas</th>
+                        <th className="w-1/12 py-2">Descarte</th>
+                        <th className="w-1/12 py-2">Operario</th>
+                        <th className="w-1/12 py-2">Observaciones</th>
+                        {rol === "Admin" ? (
+                        <>
+                            {/* De momento la edicion no va a estar disponible
+
+                            <th className="w-1/12 py-2">Editar</th> */}
+                            <th className="w-1/12 py-2">Eliminar</th>
+                        </>                  
+                        ) : null}   
+                    </tr>
+                </thead>
+                <tbody className="bg-white">
+                {data.obtenerRegistrosGE.map(registro => (
+                    <RegistroGE
+                        key={registro.id}
+                        registro={registro}
+                        rol={rol}
+                    />
+                ))}  
+                </tbody>  
             </table>
         </div>
         </Layout>
---
+++ pages/registros/nuevoregistroGE.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const NuevoRegistroGE = () => {
+
+    return (
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Nuevo Registro</h1>            
+        </Layout>
+    );
+}
+
+export default NuevoRegistroGE
