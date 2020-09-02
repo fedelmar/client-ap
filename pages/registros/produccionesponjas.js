9984bac7fd96edd1619451b6c148476eb203a044
fedelmar <fsuarez91@gmail.com>
2020-09-02 11:40:58 -0300

    agregar boton para exportar en PDF

--- pages/registros/produccionesponjas.js
+++ pages/registros/produccionesponjas.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { gql, useQuery } from '@apollo/client';
 import UsuarioContext from '../../context/usuarios/UsuarioContext';
 import Layout from '../../components/Layout';
@@ -27,7 +27,7 @@ const LISTA_REGISTROS = gql `
 const ProduccionEsponjas = () => {
 
     const { data, loading } = useQuery(LISTA_REGISTROS);
-
+    const [ pdfOpen, setPdfOpen ] = useState(false);
     const usuarioContext = useContext(UsuarioContext);
     const { rol } = usuarioContext.usuario;
 
@@ -38,15 +38,30 @@ const ProduccionEsponjas = () => {
         </Layout>
     );
 
-    //console.log(data.obtenerRegistrosCE)
+    const handleOpenClose = () => {
+      setPdfOpen(!pdfOpen);
+    }
 
+    //console.log(data.obtenerRegistrosCE)
+    console.log(pdfOpen)
     return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light" >Registros de produccion de Esponjas</h1>
 
-      <Link href="/registros/nuevoregistroPE">
-        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Iniciar producción</a>
-      </Link>
+      <div className="flex justify-between">
+        <Link href="/registros/nuevoregistroPE">
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Iniciar producción</a>
+        </Link>
+        <button onClick={() => handleOpenClose()}>
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Exportar en pdf</a>
+        </button>
+      </div>
+
+      {pdfOpen ? (
+        <div className="flex justify-end">
+          <p>Seleccione el periodo a exportar</p>
+        </div>
+      ) : null }
        
       <div className="overflow-x-scroll">
           <table className="table-auto shadow-md mt-2 w-full w-lg">
