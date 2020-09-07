97b4cc358ef511d414463c526eaf7fd3950a90b4
fedelmar <fsuarez91@gmail.com>
2020-09-07 13:41:59 -0300

    iniciar pagina de Salidas

---
+++ pages/registros/salidas.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Salidas = () => {
+    return (
+        <Layout>
+            <h1>Salidas</h1>
+        </Layout>
+    );
+}
+
+export default Salidas;
