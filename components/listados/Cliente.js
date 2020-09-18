68748bc49de98e1e80e6ea396f10c22e2060362a
fedelmar <fsuarez91@gmail.com>
2020-09-18 15:10:30 -0300

    cambiar botones de lugar

--- components/listados/Cliente.js
+++ components/listados/Cliente.js
@@ -85,15 +85,6 @@ const Cliente = ({cliente}) => {
         <tr>
             <td className="border px-4 py-2 ">{empresa}</td>
             <td className="border px-4 py-2 ">{email}</td>
-            <td className="border px-4 py-2 ">
-                <button
-                    type="button"
-                    onClick={() => confirmarEliminarCliente(id) }
-                    className="flex justify-center item-center bg-red-800 py-2 px-4 w-full text-white rounded uppercase font-bold text-xs"    
-                >
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-4 h-4"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                </button>
-            </td>
             <td className="border px-4 py-2">
                 <button
                     type="button"
@@ -101,6 +92,15 @@ const Cliente = ({cliente}) => {
                     onClick={() => editarCliente() }
                 >
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-4 h-4"><path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                </button>
+            </td>
+            <td className="border px-4 py-2 ">
+                <button
+                    type="button"
+                    onClick={() => confirmarEliminarCliente(id) }
+                    className="flex justify-center item-center bg-red-800 py-2 px-4 w-full text-white rounded uppercase font-bold text-xs"    
+                >
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-4 h-4"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 </button>
             </td>
         </tr>
