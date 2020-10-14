06b8f7d77b88b5abbfaa40445c4991f99980bb02
fedelmar <fsuarez91@gmail.com>
2020-10-14 12:56:22 -0300

    cargar registro y Manejo de stock

--- pages/registros/produccionesponjas/finalizarRegistro/[pid].js
+++ pages/registros/produccionesponjas/finalizarRegistro/[pid].js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { format } from 'date-fns';
 import { useRouter} from 'next/router';
 import { gql, useQuery } from '@apollo/client';
@@ -6,6 +6,7 @@ import Layout from '../../../../components/Layout';
 import ManejoDeStock from '../../../../components/registros/produccionesponjas/ManejoDeStock';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import UsuarioContext from '../../../../context/usuarios/UsuarioContext';
 import Swal from 'sweetalert2';
 
 const REGISTRO = gql `
@@ -21,10 +22,35 @@ const REGISTRO = gql `
     }
 `;
 
+const LISTA_STOCK_CATEGORIA = gql `
+    query obtneterStockInsumosPorCategoria($input: String!){
+        obtneterStockInsumosPorCategoria(input: $input) {
+            id
+            insumo
+            insumoID
+            cantidad
+            lote
+        }
+    }
+`;
+
 const FinalizarRegistro = () => {
 
     const router = useRouter();
     const { query: { id } } = router;
+    const usuarioContext = useContext(UsuarioContext);
+    const { productos } = usuarioContext;
+    const { nombre } = usuarioContext.usuario;
+    const { data: dataEsponjas, loading: loadingEsponjas } = useQuery(LISTA_STOCK_CATEGORIA, {
+        variables: {
+            input: "Esponjas"
+        }
+    });
+    const { data: dataBolsas, loading: loadingBolsas } = useQuery(LISTA_STOCK_CATEGORIA, {
+        variables: {
+            input: "Polietileno"
+        }
+    });
     const { data, loading } = useQuery(REGISTRO, {
         variables: {
             id
@@ -32,8 +58,13 @@ const FinalizarRegistro = () => {
     });
     const [registro, setRegistro] = useState({
         cantProducida: 0,
+        lBolsa: '',
+        lBolsaID: '',
         bolsaDisp: 0,
-        esponjaDisp: 0
+        lEsponja: '',
+        lEsponjaID: '',
+        esponjaDisp: 0,
+        cantDescarte: 0
     });
     const formikCierre = useFormik({
         initialValues: {
@@ -50,8 +81,31 @@ const FinalizarRegistro = () => {
             terminarProduccion(valores);            
         }
     });
+    useEffect(() => {
+        if(dataEsponjas && dataBolsas && data) {
+            const { obtenerRegistroCE } = data;
+            const loteEsponja = dataEsponjas.obtneterStockInsumosPorCategoria
+                                    .find(i => i.lote === obtenerRegistroCE.lEsponja);
+            const loteBolsa = dataBolsas.obtneterStockInsumosPorCategoria
+                                    .find(i => i.lote === obtenerRegistroCE.lBolsa);
+            const productoID = productos.find(i => i.nombre === obtenerRegistroCE.producto).id;
 
-    if(loading) return (
+            setRegistro({...registro,
+                lEsponja: loteEsponja.lote,
+                esponjaDisp: loteEsponja.cantidad,
+                lEsponjaID: loteEsponja.id,
+                lBolsa: loteEsponja.lote,
+                bolsaDisp: loteBolsa.cantidad,
+                lBolsaID: loteBolsa.id,
+                lote: obtenerRegistroCE.lote,
+                producto: obtenerRegistroCE.producto,
+                productoID: productoID
+
+            })
+        }
+    },[dataEsponjas, dataBolsas, data])
+   
+    if(loading || loadingEsponjas || loadingBolsas) return (
         <Layout>
           <p className="text-2xl text-gray-800 font-light" >Cargando...</p>
         </Layout>
@@ -59,7 +113,9 @@ const FinalizarRegistro = () => {
     
     const { obtenerRegistroCE } = data;
 
-    console.log(registro)
+    const terminarProduccion = valores => {
+        console.log(valores, obtenerRegistroCE)
+    }
     const cantidades = valores => {
         const {esponjas} = valores;
         setRegistro({...registro, 
@@ -89,16 +145,16 @@ const FinalizarRegistro = () => {
                         
                         <div className="flex">
                             <p className="text-gray-700 text-mm font-bold mr-1">Lote: </p>
-                            <p className="text-gray-700 font-light">{obtenerRegistroCE.lote}</p>
+                            <p className="text-gray-700 font-light">{registro.lote}</p>
                         </div>
                         <div className="flex">
                             <p className="text-gray-700 text-mm font-bold mr-1">Producto: </p>
-                            <p className="text-gray-700 font-light">{obtenerRegistroCE.producto}</p>
+                            <p className="text-gray-700 font-light">{registro.producto}</p>
                         </div>
                         <div className="flex justify-between">
                             <div className="flex">
                                 <p className="text-gray-700 text-mm font-bold mr-1">Lote de Esponja: </p>
-                                <p className="text-gray-700 font-light ">{obtenerRegistroCE.lEsponja}</p>
+                                <p className="text-gray-700 font-light ">{registro.lEsponja}</p>
                             </div>
                             <div className="flex">
                                 <p className="text-gray-700 text-mm font-bold mr-1">Disponibles: </p>
@@ -108,7 +164,7 @@ const FinalizarRegistro = () => {
                         <div className="flex justify-between pb-2">
                             <div className="flex">
                                 <p className="text-gray-700 text-mm font-bold mr-1">Lote de Bolsa: </p>
-                                <p className="text-gray-700 font-light ">{obtenerRegistroCE.lBolsa}</p>
+                                <p className="text-gray-700 font-light ">{registro.lBolsa}</p>
                             </div>
                             <div className="flex">
                                 <p className="text-gray-700 text-mm font-bold mr-1">Disponibles: </p>
