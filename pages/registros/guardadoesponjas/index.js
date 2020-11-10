3ae325580448634073e89a9eab00383a605b4c2b
fedelmar <fsuarez91@gmail.com>
2020-11-10 12:19:03 -0300

    Auth ExportPDF

--- pages/registros/guardadoesponjas/index.js
+++ pages/registros/guardadoesponjas/index.js
@@ -75,9 +75,11 @@ const GuardadoEsponjas = () => {
                     <button onClick={() => handleOpenCloseFiltros()}>
                         <a className="bg-blue-800 py-2 px-5 mt-3 mr-1 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Buscar</a>
                     </button>
-                    <button onClick={() => handleOpenClosePDF()}>
-                        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Exportar en pdf</a>
-                    </button>
+                    {rol === "Admin" ? 
+                        <button onClick={() => handleOpenClosePDF()}>
+                            <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">Exportar en pdf</a>
+                        </button>
+                    : null}
                 </div>             
             </div>
 
