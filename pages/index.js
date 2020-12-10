fa9fbaa105b26565583f3dba2200d9d13e0f0cdf
fedelmar <fsuarez91@gmail.com>
2020-12-10 09:31:44 -0300

    Ajustar espacio entre imagen y header (index)

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ const Index = () => {
   return (
     <Layout>
 
-      <div className="flex justify-center" >
+      <div className="flex justify-center mt-3" >
         <img src='/imagenAP.png' />    
       </div>
       
