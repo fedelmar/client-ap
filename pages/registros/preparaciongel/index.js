1220c09abbc72becc241ee871d511671bafbb598
fedelmar <fsuarez91@gmail.com>
2021-01-18 10:28:36 -0300

    pedir datos a DB

--- pages/registros/preparaciongel/index.js
+++ pages/registros/preparaciongel/index.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useContext } from 'react'
+import UsuarioContext from '../../../context/usuarios/UsuarioContext';
+import { gql, useQuery } from '@apollo/client';
 import Layout from '../../../components/Layout';
 
+const LISTA_REGISTROS = gql `
+    query obtenerRegistrosPG{
+        obtenerRegistrosPG{
+            id
+            creado
+            lote
+            llenado
+            cantidad
+            loteInsumo
+            tanque
+            operario
+            observaciones    
+        }
+    }
+`;
+
 const PreparacionGel = () => {
+
+    const usuarioContext = useContext(UsuarioContext);
+    const { rol } = usuarioContext.usuario;
+    const { data, loading } = useQuery(LISTA_REGISTROS, {
+      pollInterval: 500,
+    });
+    const [ pdfOpen, setPdfOpen ] = useState(false);
+    const [ filtros, setFiltros ] = useState(false);
+
+    if(loading) return (
+        <Layout>
+          <p className="text-2xl text-gray-800 font-light" >Cargando...</p>
+        </Layout>
+    );
+
+    const handleOpenClosePDF = () => {
+    setPdfOpen(!pdfOpen);
+    }
+
+    const handleOpenCloseFiltros = () => {
+    setFiltros(!filtros);
+    }
+
+    let registros = data.obtenerRegistrosPG.map(i => i);
+
+    console.log(registros)
 
     return (
         <Layout>
