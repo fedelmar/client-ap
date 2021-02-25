51ab81ea62aa132f08233694c279a188c9e68f61
fedelmar <fsuarez91@gmail.com>
2021-02-25 11:32:10 -0300

    mensaje de stock insumos en negativo

--- pages/registros/produccionesponjas/finalizarRegistro/[pid].js
+++ pages/registros/produccionesponjas/finalizarRegistro/[pid].js
@@ -278,6 +278,9 @@ const FinalizarRegistro = () => {
                                 <p className="text-gray-700 font-light">{registro.bolsaDisp}</p>
                             </div>
                         </div>
+                        {registro.esponjaDisp < 0 || registro.bolsaDisp < 0 ?
+                            <p className="text-red-700 text-mm font-bold mr-1">Disponibilidad en negativo - Revise el Stock de Insumos</p>
+                        : null}
                         
                         <div className="flex py-2">
                             <p className="text-gray-700 text-lg font-bold mr-1">Cantidad Producida: </p>
