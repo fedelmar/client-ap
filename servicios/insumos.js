807c057679b36640c5e14099eb9322cf4f22448c
Federico Suarez <fsuarez@intermediait.com>
2022-07-26 09:47:32 -0300

    feat add insumos faltantes

--- servicios/insumos.js
+++ servicios/insumos.js
@@ -1,18 +1,25 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-const OBTENER_STOCK_CATEGORIA = gql `
-    query obtneterStockInsumosPorCategoria($input: String!){
-        obtneterStockInsumosPorCategoria(input: $input) {
-            id
-            insumo
-            insumoID
-            cantidad
-            lote
-        }
+const OBTENER_STOCK_CATEGORIA = gql`
+  query obtneterStockInsumosPorCategoria($input: String!) {
+    obtneterStockInsumosPorCategoria(input: $input) {
+      id
+      insumo
+      insumoID
+      cantidad
+      lote
     }
+  }
 `;
 
+const OBTENER_INSUMOS_FALTANTES = gql`
+  query obtenerInsumosFaltantes {
+    obtenerInsumosFaltantes {
+      insumo
+      cantidad
+      categoria
+    }
+  }
+`;
 
-export {
-  OBTENER_STOCK_CATEGORIA,
-};
+export { OBTENER_STOCK_CATEGORIA, OBTENER_INSUMOS_FALTANTES };
